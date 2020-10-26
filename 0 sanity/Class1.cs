58991afc23b6b49ca539c47edc3a6237863d9cd8
kenx00x <kenx00x@gmail.com>
2020-10-26 13:18:01 +0100

    Add project files.

---
+++ 0 sanity/Class1.cs
@@ -0,0 +1,19 @@
+﻿using MelonLoader;
+
+namespace _0_sanity
+{
+    public class Class1 : MelonMod
+    {
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+            GameController.instance.myPlayer.player.insanity = 100;
+
+        }
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+            MelonLogger.Log("0 sanity mod loaded!");
+        }
+    }
+}
