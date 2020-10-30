7c963a45c445fb40e12475034cb424da852d4778
kenx00x <kenx00x@gmail.com>
2020-10-30 17:42:46 +0100

    fixed mod

--- 0 sanity/Class1.cs
+++ 0 sanity/Class1.cs
@@ -1,11 +1,18 @@
 ﻿using MelonLoader;
+
 namespace _0_sanity
 {
     public class Class1 : MelonMod
     {
         public override void OnUpdate()
         {
-            GameController.instance.myPlayer.player.insanity = 100;
+            //unobfuscated
+            //GameController.instance.myPlayer.player.insanity = 100;
+            //obfuscared but due to a bug currently unusable
+            ////GameController.field_Public_Static_GameController_0.field_Public_ObjectPublicPlStPlUnique_0.field_Public_Player_0.field_Private_Single_1 = 100;
+
+            Player LocalPlayer = Helpers.GetLocalPlayer();
+            LocalPlayer.field_Public_Single_0 = 100;
         }
         public override void OnApplicationStart()
         {
