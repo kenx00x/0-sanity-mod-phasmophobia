37bf92bafe0ce702c37fd6e202d5680adc976037
kenx00x <kenx00x@gmail.com>
2020-10-31 10:35:20 +0100

    removed empty lines

--- 0 sanity/Class1.cs
+++ 0 sanity/Class1.cs
@@ -1,5 +1,4 @@
 ﻿using MelonLoader;
-
 namespace _0_sanity
 {
     public class Class1 : MelonMod
@@ -10,7 +9,6 @@ namespace _0_sanity
             //GameController.instance.myPlayer.player.insanity = 100;
             //obfuscared but due to a bug currently unusable
             ////GameController.field_Public_Static_GameController_0.field_Public_ObjectPublicPlStPlUnique_0.field_Public_Player_0.field_Public_Single_0 = 100;
-
             Player LocalPlayer = Helpers.GetLocalPlayer();
             LocalPlayer.field_Public_Single_0 = 100;
         }
